16daa9e9d1c7f945cb53c5ca97beb6b04891f0ac
Tre <tregallowayofficial@gmail.com>
2023-07-22 15:01:29 -0500

    Basic layout for homepage

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+import { SiteConfig } from "@/types"
+
+export const siteConfig: SiteConfig = {
+  name: "MyDocs",
+  description:
+    "An application for notes presented in documentation form (for now)",
+  url: "",
+  ogImage: "",
+  links: {
+    twitter: "https://twitter.com/bytregalloway",
+    github: "https://github.com/TreGalloway/MyDocs",
+  },
+}
--- types/index.d.ts
+++ types/index.d.ts
@@ -29,3 +29,13 @@ export type NavItem = {
   export type HomeConfig = {
     mainNav: MainNavItem[]
   }
+  export type SiteConfig = {
+    name: string
+    description: string
+    url: string
+    ogImage: string
+    links: {
+      twitter: string
+      github: string
+    }
+  }
